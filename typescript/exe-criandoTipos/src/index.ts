f6f539c6838c35ad56f9de880997df4821b04c2d
gustavopesso <gustavopg8989@gmail.com>
2024-03-04 15:08:02 -0300

    estudos

--- typescript/exe-criandoTipos/src/index.ts
+++ typescript/exe-criandoTipos/src/index.ts
@@ -1,5 +1,1 @@
-function objectPlanet(name: string, coordenadas: [number,number,number,number], situacao: string|string|string|string, satelites: string){
-    let planet = {
-        name
-    }
-}
+let coo
