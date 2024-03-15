34e5a1d1845cfbc5770d12aa87b4e350fdccc480
gustavopesso <gustavopg8989@gmail.com>
2024-03-15 15:41:59 -0300

    estudos

--- javascript/javascript-DOM/exe11-escalacaoDeTimes/index.js
+++ javascript/javascript-DOM/exe11-escalacaoDeTimes/index.js
@@ -1,6 +1,42 @@
 function addPlayer(){
-    const lisPlayer = document.getElementById('listPlayers')
+    const listPlayer = document.getElementById('escalPlayer')
 
-    const namePlayer = prompt('Informe o nome do jogador')
-    const positionPlayer = prompt('Informe a posição do jogador')
+    const h3 = document.createElement('h3')
+    h3.innerText = "Perfil do jogador:"
+
+    const ul = document.createElement('ul')
+
+    const positionLi = document.createElement('li')
+    positionLi.innerText = "Posição:"
+    const positionInput = document.createElement('input')
+    positionInput.type = 'text'
+    positionInput.name = 'fullname'
+    positionLi.appendChild(positionInput)
+    ul.appendChild(positionLi)
+
+    const nameLi = document.createElement('li')
+    nameLi.innerText = "Nome:"
+    const nameInput = document.createElement('input')
+    nameInput.type = 'text'
+    nameInput.name = 'fullname'
+    nameLi.appendChild(nameInput)
+    ul.appendChild(nameLi)
+
+    const numberLi = document.createElement('li')
+    numberLi.innerText = "Camisa:"
+    const numberInput = document.createElement('input')
+    numberInput.type = 'number'
+    numberInput.name = 'fullname'
+    numberLi.appendChild(numberInput)
+    ul.appendChild(numberLi)
+
+    const btnPlayer = document.createElement('button')
+    btnPlayer.innerText = "Escalar"
+    ul.appendChild(btnPlayer)
+    btnPlayer.addEventListener("click")
+    if(btnPlayer){
+        let confirmation = confirm("deseja escalar esse jogador?")
+    }
+
+    listPlayer.append(h3,ul)
 }
