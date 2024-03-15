6ec1b84a5fa74bf288c9e8c0ce7e4654124d9ebf
gustavopesso <gustavopg8989@gmail.com>
2024-03-15 14:48:14 -0300

    estudos

---
+++ javascript/javascript-DOM/exe11-escalacaoDeTimes/index.js
@@ -0,0 +1,6 @@
+function addPlayer(){
+    const lisPlayer = document.getElementById('listPlayers')
+
+    const namePlayer = prompt('Informe o nome do jogador')
+    const positionPlayer = prompt('Informe a posição do jogador')
+}
