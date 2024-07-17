054df790861cb7d4ad05a0c57eb538e85d319c7a
gustavopesso <gustavopg8989@gmail.com>
2024-07-17 15:25:54 -0300

    estudos

--- javascript/javascript Moderno/exe01-media/index.js
+++ javascript/javascript Moderno/exe01-media/index.js
@@ -1,1 +1,6 @@
-const mediaSimples = ('34','54','36') =>{ }
+const average = (...numbers) =>{
+    const sum = numbers.reduce((accum, num) => accum + num, 0)
+    return sum / numbers.length
+}
+console.log(`Média Aritimetica simples: ${average(3,4,34,1,5)} `)
+
